074aa29fcd12b8c87da0237888037f0af862618b
ctylim <lim.0.ctyl@gmail.com>
2019-07-31 17:36:35 +0900

    test randomness of permutation

--- src/shuffle.rs
+++ src/shuffle.rs
@@ -7,11 +7,33 @@ pub fn fisher_yates_shuffle_n(n: usize) -> Vec<usize> {
         v.push(i);
     }
     let mut rng = thread_rng();
-    for i in (0..n).rev() {
-        if i > 0 {
-            let r: usize = rng.gen();
-            v.swap(i, r % (i + 1));
-        }
+    for i in (1..n).rev() {
+        let r: usize = rng.gen();
+        v.swap(i, r % (i + 1));
     }
     v
+}
+
+#[test]
+fn fisher_yates_shuffle_chi_square_test() {
+    let mut cnt = [[0u32; 10]; 10];
+    for _ in 0..10000 {
+        let v = fisher_yates_shuffle_n(10);
+        for i in 0..10 {
+            cnt[i][v[i]] += 1;
+        }
+    }
+    let chis: Vec<f32> = cnt
+        .iter()
+        .map(|x| {
+            let mut a = 0f32;
+            for v in x.iter() {
+                a += (*v as f32 - 1000f32) * (*v as f32 - 1000f32) / 1000f32;
+            }
+            a
+        })
+        .collect();
+    for chi in chis {
+        assert!(chi < 25.2);
+    }
 }
